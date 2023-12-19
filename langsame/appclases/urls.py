0761dafbab5d9e03a4467c0c2b504bd0488fe774
Lucas Gallo <lucasgallo@gmail.com>
2023-12-19 19:19:35 -0300

    navigating

--- langsame/appclases/urls.py
+++ langsame/appclases/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from appclases.views import *
 
 urlpatterns = [
-    path("", inicio),
-    path("clases/", clases),
-    path("profesores/", profesores),
-    path("alumnos/", alumnos)
+    path("", inicio, name="inicio"),
+    path("clases/", clases, name="clases"),
+    path("profesores/", profesores, name="profesores"),
+    path("alumnos/", alumnos, name="alumnos"),
+    path("entregables/", entregables, name="entregables")
 ]
--- langsame/appclases/views.py
+++ langsame/appclases/views.py
@@ -4,19 +4,26 @@ from appclases.models import Clase
 
 # Create your views here.
 def inicio(request):
-    return render(request, "appclases/base.html")
+    return render(request, "appclases/index.html")
 
 def clases(request):
-    clases = Clase.objects.all()
-    
-    cadena_respuesta = ""
-    for clase in clases:
-        cadena_respuesta += f"Clase: {clase.nombre} - Comisión: {clase.comision}" + " " +"<br/>"
-    
-    return HttpResponse(cadena_respuesta) 
+    return render(request, "appclases/clases.html")
 
 def profesores(request):
     return render(request, "appclases/profesores.html")
 
 def alumnos(request):
     return render(request, "appclases/alumnos.html")
+
+def entregables(request):
+    return render(request, "appclases/entregables.html")
+
+
+# def clases(request):
+    #clases = Clase.objects.all()
+    
+    #cadena_respuesta = ""
+    #for clase in clases:
+    #    cadena_respuesta += f"Clase: {clase.nombre} - Comisión: {clase.comision}" + " " +"<br/>"
+    
+    #return HttpResponse(cadena_respuesta)
