c8cde9e0145dec8742c96e64ea50a589bd2edb36
Lucas Gallo <lucasgallo@gmail.com>
2023-12-20 19:23:01 -0300

    admin panel

--- langsame/appclases/admin.py
+++ langsame/appclases/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from appclases.models import *
+
+admin.site.register(Clase)
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Entregable)
